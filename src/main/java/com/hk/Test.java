5ad1ecf2c20b3239dac0d18c664e1fecd331c90a
tiaotiaochong <15691644441@163.com>
2019-11-10 20:29:39 +0800

    add  test

--- src/main/java/com/hk/Test.java
+++ src/main/java/com/hk/Test.java
@@ -8,4 +8,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("123");
     }
+
+    public static void tset(){
+        System.out.println("123");
+    }
 }
