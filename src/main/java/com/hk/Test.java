ccc29cbcfc2cdf6617349a4683f49d6f2d529889
tiaotiaochong <15691644441@163.com>
2019-11-10 20:24:15 +0800

    main

---
+++ src/main/java/com/hk/Test.java
@@ -0,0 +1,11 @@
+package com.hk;
+
+/**
+ * @author tiaotiaochong
+ * @data 2019/11/10
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
